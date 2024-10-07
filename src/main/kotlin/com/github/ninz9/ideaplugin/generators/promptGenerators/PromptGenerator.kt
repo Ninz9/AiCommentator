bcf7f718a29e2964d55f836daa141def1f96faaf
Roman Chertishchev <rchertishchev@gmail.com>
2024-10-08 01:10:18 +0200

    refactor: change prompts, llm returns invalid requests

--- src/main/kotlin/com/github/ninz9/ideaplugin/generators/promptGenerators/PromptGenerator.kt
+++ src/main/kotlin/com/github/ninz9/ideaplugin/generators/promptGenerators/PromptGenerator.kt
@@ -7,12 +7,19 @@ import com.intellij.openapi.components.Service
 @Service
 class PromptGenerator {
 
-    val typePrompt = "Create a comment in JavaDoc format for the provided code snippet."
+    private val typePrompt = "Please generate a valid documentation comment for the provided method." +
+            " Ensure that the response contains only the comment text, using the appropriate documentation" +
+            " format. The comment should explain the method’s purpose, key input parameters, and return value" +
+            " if applicable. Avoid any additional formatting such as code blocks, backticks, or extra characters," +
+            " as the comment will be automatically inserted into the code and must be syntactically correct." +
+            " Start message with /** and end with */."
+
+
 
     fun generatePromptForMethod(codeStructure: CodeStructure): Collection<ModelMessage> {
 
         val rolePrompt =
-            "You are acting as an assistant to generate comments for class methods. Your task is to create a brief and precise comment for the provided method. The comment should explain the method’s purpose, key input parameters, and return value if applicable. The response should contain only the comment and nothing else. This comment will be automatically inserted into the code, so avoid additional explanations or details."
+            "You are acting as an assistant to generate comments for class methods. Your task is to create a brief and precise comment for the provided method in JavaDoc format. The comment should explain the method’s purpose, key input parameters, and return value if applicable. The response should contain only the comment text, without any additional formatting such as code blocks, backticks, or extra characters. This comment will be automatically inserted into the code, so avoid additional explanations or details."
 
         return listOf(
             ModelMessage(
