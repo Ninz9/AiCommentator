7006f9369f22c74cee26023e0af2552f232730ed
Chertishchev Roman <rchertishchev@gmail.com>
2024-10-08 11:18:49 +0200

    fix: restructured JSON for Anthropic API request

--- src/main/kotlin/com/github/ninz9/ideaplugin/llm/modelsImpl/anthropic/AnthropicClient.kt
+++ src/main/kotlin/com/github/ninz9/ideaplugin/llm/modelsImpl/anthropic/AnthropicClient.kt
@@ -25,7 +25,7 @@ class AnthropicClient(
 
     override suspend fun sendRequest(messages: Collection<ModelMessage>): String {
 
-        val requestBody = buildJsonRequestBody(messages, false)
+        val requestBody = buildJsonRequestBody(messageAdapter(messages), false)
         val headers = mapOf(
             "anthropic-version" to "2023-06-01",
             "x-api-key" to token,
@@ -42,25 +42,42 @@ class AnthropicClient(
 
         when(response) {
             is ApiResponse.Error -> throw Exception(response.error.error.message)
-            is ApiResponse.Success -> return response.data.content.first().text
+            is ApiResponse.Success -> {
+                if (response.data.content.isEmpty()) {
+                    throw Exception("No messages returned")
+                }
+                return response.data.content.first().text
+            }
         }
     }
 
 
-    private fun buildJsonRequestBody(messages: Collection<ModelMessage>, stream: Boolean): JSONObject {
+    private fun buildJsonRequestBody(messages: Map<String, Collection<String>>, stream: Boolean): JSONObject {
         val json = JSONObject()
-        val messagesJson = messages.map {
-            JSONObject().apply {
-                put("role", it.role)
-                put("content", it.message)
-            }
-        }
+        val messagesJson = listOf(
+            JSONObject().put("role", "user").put("content", messages["user"]?.map {
+                JSONObject().put("type", "text").put("text", it)
+            } ?: listOf<String>()),
+        )
+        json.put("system", messages["assistant"]?.first() ?: "")
         json.put("messages", messagesJson)
         json.put("model", model.modelName)
         json.put("max_tokens", maxTokens)
         json.put("temperature", temperature)
         json.put("stream", stream)
         return json
+    }
+
+    private fun  messageAdapter(messages: Collection<ModelMessage>): Map<String, Collection<String>> {
+        val mergedMessages = mutableMapOf<String, MutableList<String>>()
+        messages.forEach {
+            if (mergedMessages.containsKey(it.role)) {
+                mergedMessages[it.role] = mergedMessages[it.role]!!.apply { add(it.message) }
+            } else {
+                mergedMessages[it.role] = mutableListOf(it.message)
+            }
+        }
+        return mergedMessages
     }
 
 }
