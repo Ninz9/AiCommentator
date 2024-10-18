c7d434010f466f8e0ef458b40016593a974ff87e
Chertishchev Roman <rchertishchev@gmail.com>
2024-10-18 12:46:22 +0200

    refactor: PromptGenerator to handle language-specific prompts.
    Split the prompt generation logic into separate prompts for methods and classes. Added helper function `getCommentPromptForLanguage` to provide language-specific comment structures for Java and Kotlin. This enhances the flexibility and readability of the code.

--- src/main/kotlin/com/github/ninz9/ideaplugin/generators/promptGenerators/PromptGenerator.kt
+++ src/main/kotlin/com/github/ninz9/ideaplugin/generators/promptGenerators/PromptGenerator.kt
@@ -7,43 +7,66 @@ import com.intellij.openapi.components.Service
 @Service
 class PromptGenerator {
 
-    private val typeStreamPrompt = "Your task is to generate code comments in KDoc, JavaDoc format. You will be provided with code for a method or a class." +
-            " For methods, generate a comment that includes:\n" +
-            "- A brief description of the method's purpose\n" +
-            "- @param for each parameter (if any)\n" +
-            "- @return with a description of the return value (if the method returns something)\n" +
-            "- @throws with a description of exceptions (if the method can throw any)\n" +
-            "For classes, generate a comment that includes:\n" +
-            "- A brief description of the class's purpose\n" +
-            "- @property for each class property\n" +
-            "- @constructor for the constructor (if present)\n" +
-            "Output only the ready-to-use comment without any additional explanations or surrounding text. \n" +
-            "Do not include any code snippets in the comment, and do not include any annotations other than the ones mentioned above."
+
+    private val methodCommentPrompt = """
+                    Your task is to generate code comments for a given method. You will be provided with the method code.
+                    
+                    
+                    Output only the ready-to-use comment without any additional explanations or surrounding text.
+                    Do not include any code snippets in the comment, and do not include any annotations other than the ones mentioned above."""
+
+    private val classCommentPrompt = """
+                    Your task is to generate code comments for a given class. You will be provided with the class code.
+
+                    Output only the ready-to-use comment without any additional explanations or surrounding text.
+                    Do not include any code snippets in the comment, and do not include any annotations other than the ones mentioned above.
+                    Do not describe methods inside the class."""
+
+    private val javaCommentPrompt = """
+                Do not return example code, do not use @author or @version or @since tags.
+                DO NOT generate example usage.
+                DO NOT generate usage example.
+                DO NOT use html tags such as <p>, <lu>, <li>.
+                DO NOT generate documentation for type member properties.
+                Write javadoc."""
+
+    private val kotlinCommentPrompt = """
+                Do not return example code, do not use @author or @version or @since tags.
+                DO NOT generate example usage.
+                DO NOT generate usage example.
+                DO NOT use html tags such as <p>, <lu>, <li>.
+                DO NOT generate documentation for type member properties.
+                Write KDoc."""
 
     fun generatePromptForMethod(codeStructure: CodeStructure): Collection<ModelMessage> {
 
         return listOf(
+            getCommentPromptForLanguage(codeStructure.language),
             ModelMessage(
-                "assistant",
-                typeStreamPrompt
-            ),
-            ModelMessage(
-                "user",
-                codeStructure.code
+                "assistant", methodCommentPrompt
+            ), ModelMessage(
+                "user", codeStructure.code
             )
         )
     }
-    fun generatePromptForClass(codeStructure: CodeStructure): Collection<ModelMessage> {
 
+    fun generatePromptForClass(codeStructure: CodeStructure): Collection<ModelMessage> {
         return listOf(
+            getCommentPromptForLanguage(codeStructure.language),
             ModelMessage(
-                "assistant",
-                typeStreamPrompt
-            ),
-            ModelMessage(
-                "user",
-                codeStructure.code
+                "assistant", classCommentPrompt
+            ), ModelMessage(
+                "user", codeStructure.code
             )
         )
+    }
+
+
+    private fun getCommentPromptForLanguage(language: String): ModelMessage {
+        return when (language) {
+            "Java" -> ModelMessage("assistant", javaCommentPrompt)
+            "Kotlin" -> ModelMessage("assistant", kotlinCommentPrompt)
+            else -> ModelMessage("assistant", "")
+        }
     }
 }
